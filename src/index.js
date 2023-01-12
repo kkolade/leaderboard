585025f86392adb81b95a90024e9236126ca919d
Kola Kolade <kolakolade@gmail.com>
2023-01-12 23:30:12 +0100

    async funtion to create a new game created

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import './css/reset.css';
+import { gameID } from './modules/initGame';
 import './scss/main.scss';
+
+console.log(gameID);
---
+++ src/modules/initGame.js
@@ -0,0 +1,28 @@
+const newGame = {
+  name: 'Lovely Game',
+};
+
+const option = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(newGame),
+};
+const myGame = async () => {
+  const response = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/',
+    option
+  );
+
+  if (!response.ok) {
+    throw new Error(console.log(response.status));
+  } else {
+    const data = await response.json();
+    return data;
+  }
+};
+
+const gameID = myGame();
+
+export { gameID };
