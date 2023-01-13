94d253192e06bdf7b9c64fdd8250465824abb5ec
Kola Kolade <kolakolade@gmail.com>
2023-01-13 12:16:11 +0100

    Implement add score

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './css/reset.css';
+import Leaderboard from './modules/leaderboard.js';
+import View from './modules/view.js';
 import './scss/main.scss';
 
 const name = document.querySelector('#name');
@@ -9,3 +11,32 @@ const refreshBtn = document.querySelector('.refresh');
 const url =
   'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/npI1n6Hh3zxhpmmzNdO1';
 const api = new Leaderboard(url);
+
+// On Page Load
+const requestUrl = `${url}/scores/`;
+api.getScore(requestUrl);
+
+setTimeout(() => {
+  document.addEventListener('DOMContentLoaded', () => {
+    api.getScore(requestUrl);
+  });
+}, 1000);
+
+const view = new View();
+
+// Submit Button Event
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  api.addScore(requestUrl, name.value.trim(), score.value);
+  submitBtn.setCustomValidity('"Leaderboard score created correctly.');
+  submitBtn.reportValidity();
+  name.value = '';
+  score.value = '';
+
+  setTimeout(() => {
+    api.getScore(requestUrl);
+    const arr = JSON.parse(localStorage.getItem('data'));
+    view.scoreList(arr);
+  }, 1000);
+});
